614930f8da15217c86b31860294900cb66497a83
Joakim Bech <joakim.bech@linaro.org>
2015-01-13 23:08:03 +0100

    Updates for building statically compiled driver
    This update make it possible to build using the first try to put OP-TEEs
    kernel driver into the kernel instead of having it as a dynmically
    loadable, out of tree driver.
    
    This also use the reworked driver coming from ST that could be found
    here:
    https://github.com/ajs-sun/linux/commits/upstream
    and here:
    https://github.com/OP-TEE/optee_linuxdriver/pull/7
    
    Signed-off-by: Joakim Bech <joakim.bech@linaro.org>

--- flags.mk
+++ flags.mk
@@ -13,7 +13,7 @@ CFLAGS          := -Wall -Wbad-function-cast -Wcast-align \
 		   -Wmissing-noreturn -Wmissing-prototypes -Wnested-externs \
 		   -Wpointer-arith -Wshadow -Wstrict-prototypes \
 		   -Wswitch-default -Wunsafe-loop-optimizations \
-		   -Wwrite-strings -Werror
+		   -Wwrite-strings
 CFLAGS          += -c -fPIC
 
 DEBUG       ?= 0
